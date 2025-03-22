e6e94ce5e2fbed9e975caf78e0f253f572b002a0
Sharda <shardachawre96@gmail.com>
2025-03-22 21:47:23 +0530

    Basic setup

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated,AllowAny
 # Create your views here.
+
+class CreateUSerview(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
